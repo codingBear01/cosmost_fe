dfd02f47b8b5c524868859a5d9dc566f3a2e38c3
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-09-26 18:29:17 +0900

    [create] 검색 결과 페이지 생성

---
+++ src/components/pages/SearchResultPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '../common';
+
+function SearchResultPage() {
+  return (
+    <div>
+      <Header></Header>
+      여기는 검색 결과 페이지이무니닷
+    </div>
+  );
+}
+
+export default SearchResultPage;
