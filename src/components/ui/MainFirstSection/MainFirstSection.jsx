aa356259cf897930886ab45b68454daeaf1cedf1
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-09-27 17:58:11 +0900

    [update] RankingData 경로 수정

--- src/components/ui/MainFirstSection/MainFirstSection.jsx
+++ src/components/ui/MainFirstSection/MainFirstSection.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import * as S from './styled';
 import { Button, Section, SmallProfilePic } from '../../';
 import { COLOR_LIST as color, FONT_SIZE_LIST as fs } from './../../../style/';
-import { USER_RANKING_LIST } from './../../../store/data/RankingData';
+import { USER_RANKING_LIST } from './../../../store/data/';
 import { useInterval } from './../../hooks/';
 
 function MainFirstSection() {
