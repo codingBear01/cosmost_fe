3de17cff67ad6914abd3e64f251403d98350e9ef
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-10-05 10:07:21 +0900

    [create] UserPage 메뉴 리스트 생성

--- src/components/ui/UserPageForm/UserPageForm.jsx
+++ src/components/ui/UserPageForm/UserPageForm.jsx
@@ -1,4 +1,6 @@
+/* libraries */
 import React from 'react';
+import { Link } from 'react-router-dom';
 /* components */
 import * as S from './styled';
 import { UserProfilArea } from '.';
@@ -12,12 +14,26 @@ import {
 } from '../..';
 /* static data */
 import { COLOR_LIST as color, FONT_SIZE_LIST as fs } from '../../../style';
+import { USER_PAGE_MENU_LIST as menu } from '../../../data';
 
 function UserInfoForm() {
   return (
     <UtilDiv pd={'5rem'}>
       <UtilTitle>닉네임 님</UtilTitle>
+      {/* 유저 프로필 및 유저 정보, 프로필 편집 버튼 */}
       <UserProfilArea />
+      {/* 유저 페이지 메뉴 목록 */}
+      <S.UserPageMenuList>
+        {menu &&
+          menu.map((item) => (
+            <Link key={item.id} to={item.path}>
+              <S.UserPageMenuItem>
+                {item.icon}
+                <span>{item.title}</span>
+              </S.UserPageMenuItem>
+            </Link>
+          ))}
+      </S.UserPageMenuList>
     </UtilDiv>
   );
 }
--- src/data/index.js
+++ src/data/index.js
@@ -4,3 +4,4 @@ export { USER_RANKING_LIST } from './RankingData';
 export { REPORT_CATEGORIES_LIST } from './ReportCategoriesData';
 export { MAIN_COURSE_LIST } from './MainCoursesData';
 export { LOGIN_BTN_LIST } from './LoginBtnsData';
+export { USER_PAGE_MENU_LIST } from './UserPageMenuData';
