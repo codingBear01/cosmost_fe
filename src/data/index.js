c0d0423d5e9c46011ada777f6fb1059d60c0e832
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-10-05 10:48:03 +0900

    [create] 회원가입 페이지 프로필 업로드 및 미리보기 관련 ref, input button 생성

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,3 @@
-export { AgreementPageData } from './AgreementPageData';
 export { HASH_TAG_LIST } from './MainHashTags';
 export { USER_RANKING_LIST } from './RankingData';
 export { REPORT_CATEGORIES_LIST } from './ReportCategoriesData';
