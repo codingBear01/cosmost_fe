e32612b91455943518ce01a122d787c2bed25e98
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-10-06 22:46:28 +0900

    [update] 메인 페이지 코스 padding 값 수정

--- src/components/ui/MainSection/MainSection.jsx
+++ src/components/ui/MainSection/MainSection.jsx
@@ -25,7 +25,7 @@ function MainPageSection() {
         {/* 메인 코스 */}
         {MAIN_COURSES &&
           MAIN_COURSES.map((item, i) => (
-            <MainContentWrap key={item.id} pd={'0 10rem'}>
+            <MainContentWrap key={item.id} padding={'0 10rem'}>
               <MainCourse item={item} />
             </MainContentWrap>
           ))}
