81f92254b0155ad7f13e325c1773c49b777bf55f
InvincibleGaren <86401105+InvincibleGaren@users.noreply.github.com>
2022-11-06 17:01:24 +0900

    #6 [create] 네이버 로그인 API 일부 구현
    #6 [create] 네이버 로그인 API 일부 구현

--- src/components/ui/LoginForm/LoginForm.jsx
+++ src/components/ui/LoginForm/LoginForm.jsx
@@ -93,12 +93,14 @@ function LoginForm() {
     var naver_id_login = new window.naver_id_login(process.env.REACT_APP_X_NAVER_CLIENT_ID, "http://localhost:3000");
     var state = naver_id_login.getUniqState();
     naver_id_login.setButton("white", 2,40);
-    naver_id_login.setDomain("YOUR_SERVICE_URL");
+    naver_id_login.setDomain("http://localhost:3000");
     naver_id_login.setState(state);
-    naver_id_login.setPopup();
+    // naver_id_login.setPopup();
     naver_id_login.init_naver_id_login();
-  },[])
 
+
+  },[])
+ 
   return (
     <UtilForm
       justifyContent={'center'}
