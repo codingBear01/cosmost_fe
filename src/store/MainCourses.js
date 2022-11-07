940bc3e47a9d2fcded4728ee4d0d1399b9691a38
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-11-07 11:41:36 +0900

    [update] main courses 최근 추가된 코스 삭제

--- src/store/MainCourses.js
+++ src/store/MainCourses.js
@@ -1,7 +1,6 @@
 /* icons */
 import * as GiIcons from 'react-icons/gi';
 import * as FiIcons from 'react-icons/fi';
-import * as MdIcons from 'react-icons/md';
 
 export const MAIN_COURSES = [
   {
@@ -23,13 +22,6 @@ export const MAIN_COURSES = [
     alignSelf: 'start',
     title: '높은 평가 받은 코스',
     icon: <FiIcons.FiStar />,
-    path: '/searched-courses',
-  },
-  {
-    id: 4,
-    alignSelf: 'end',
-    title: '최근 추가된 코스',
-    icon: <MdIcons.MdOutlineAlarmOn />,
     path: '/searched-courses',
   },
 ];
